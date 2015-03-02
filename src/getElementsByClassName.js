ec97a39a202ab145d20dfdd8b38f85c6b87de6be
Darren Wong <darrenwong23@gmail.com>
2015-03-01 22:40:30 -0800

    Complete stringifyJSON.js and getElementsByClassName.js

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,5 +6,16 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ){
-  // your code here
+	var result = [];
+	return helper(className.split(' '), [document.body]);
+
+	function helper(classNames, segment) {
+		_.each(segment, function(element){
+			if(_(classNames).intersection(element.classList).length === classNames.length) result.push(element);
+			helper(classNames,element.childNodes);
+		});
+
+		return result;
+	}
 };
+
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,7 +2,29 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
-
 var stringifyJSON = function(obj) {
-  // your code goes here
+
+	var result = [];
+
+	//cases: array
+	if(Array.isArray(obj)){
+		result = obj.map(function(x){return self.stringifyJSON(x)});
+		return "[" +result.join() + "]";
+	}
+
+	//cases: object (cannot be null)
+	if(obj && typeof obj === "object"){
+		for(var key in obj) {
+			if(typeof obj[key] !== "undefined" && typeof obj[key] !== "function") {
+				result.push( self.stringifyJSON(key)+":"+self.stringifyJSON(obj[key]));  	
+			}
+		}
+		return "{" + result.join() + "}";
+	}
+
+	//cases: String
+	if(typeof obj === "string") return "\"" +obj + "\"";
+
+	//cases: Null, Boolean, Number
+	return String(obj);
 };
